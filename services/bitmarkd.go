b24d41079ddfade5f16e19018705ab95785f44d9
Yun-Tai Chang <yun-tai@bitmark.com>
2016-06-30 17:15:39 +0800

    use different way to stop bitmarkd

--- services/bitmarkd.go
+++ services/bitmarkd.go
@@ -186,10 +186,13 @@ func (bitmarkd *Bitmarkd) stopBitmarkd() error {
 		return fault.ErrBitmarkdIsNotRunning
 	}
 
-	if err := bitmarkd.process.Kill(); nil != err {
-		bitmarkd.log.Errorf("Send interrupt to bitmarkd failed: %v", err)
-		return err
-	}
+	// if err := bitmarkd.process.Signal(os.Interrupt); nil != err {
+ 		// bitmarkd.log.Errorf("Send interrupt to bitmarkd failed: %v", err)
+		if err := bitmarkd.process.Signal(os.Kill); nil != err {
+			bitmarkd.log.Errorf("Send kill to bitmarkd failed: %v", err)
+			return err
+		}
+ 	// }
 
 	bitmarkd.log.Infof("Stop bitmarkd. PID: %d", bitmarkd.process.Pid)
 	bitmarkd.running = false
