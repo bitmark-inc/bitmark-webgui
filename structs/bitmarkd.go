4dbae2e766af35b12fa49846b8e8b0ba5ad99f07
Jim Yeh <jim@bitmark.com>
2017-04-19 17:17:50 +0800

    Truncate files when save configuration

--- structs/bitmarkd.go
+++ structs/bitmarkd.go
@@ -64,7 +64,7 @@ type BitmarkdConfiguration struct {
 }
 
 func (b *BitmarkdConfiguration) SaveToJson(filename string) error {
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0600)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		return err
 	}
